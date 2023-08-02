b7c2603ad124fac75de03f60945928f5319dbd05
Logan Long <loganjohnlong@users.noreply.github.com>
2023-08-01 20:57:27 -0500

    Remove broken command

--- assets/startup.sh
+++ assets/startup.sh
@@ -33,8 +33,5 @@ fi
     echo "ntsdumpdir /var/lib/chrony"
 } > ${CHRONY_CONF_FILE}
 
-# Disable synchronization with NTP servers provided by DHCP
-echo "PEERNTP=no" >> ${NETWORK_SYS_CONFIG}
-
 # startup chronyd in the foreground
 exec /usr/sbin/chronyd -u chrony -d -x -L 0
